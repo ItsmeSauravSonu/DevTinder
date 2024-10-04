15ce7d965c8aa316feb4e37d008da10d283bfd89
SAURAV KUMAR SONU <sauravsonu2310@gmail.com>
2024-10-04 23:14:16 +0530

    we can write in form of array also

--- src/app.js
+++ src/app.js
@@ -2,20 +2,38 @@ const express = require("express");
 
 const app = express();
 
+// ap.use("/router", rh1, rh2, [rh3,rh4])
+
 app.use(
   "/user",
+  [
   (req, res,next) => {
     //Route Handler
     //res.send("Route Handler 1");
     console.log("Handling the route user!!");
     
     next();
-    res.send("Response!");
+    //res.send("Response!");
   },
-  (req, res) => {
+  (req, res, next) => {
     //Route Handler 2
     console.log("Handling the route user2!!");
-    res.send("2nd Response!");
+    //res.send("2nd Response!");
+    next();
+  }
+]
+  ,
+  (req, res, next) => {
+    //Route Handler 3
+    console.log("Handling the route user3!!");
+    //res.send("2nd Response!");
+    next();
+  },
+  (req, res, next) => {
+    //Route Handler 4
+    console.log("Handling the route user4!!");
+    res.send("4th Response!");
+    //next();
   }
 );
 
