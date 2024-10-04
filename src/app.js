1caa3dcc3eeb6300308ce55cd23b48433cfb0bd3
SAURAV KUMAR SONU <sauravsonu2310@gmail.com>
2024-10-04 23:43:13 +0530

    why do we need middlewares

--- src/app.js
+++ src/app.js
@@ -2,33 +2,21 @@ const express = require("express");
 
 const app = express();
 
-
-// GET /users  => It checks all the app.xxx('matching route') functions 
-// middlewareChain => request handler
-
-
-app.get(
-  "/user",
-  (req, res,next) => {
-    console.log("Handling the route user!!");
-    next();
-  },
-  (req, res,next) => {
-    console.log("Handling the route user!!");
-    next();
-  },
-  (req, res,next) => {
-    console.log("Handling the route user!!");
-    next();
-  },
-  (req, res,next) => {
-    console.log("Handling the route user!!");
-    res.send("Response!!!......")
+app.get("/admin/getAllData",(req, res)=>{
+  //Logic of Check if the request is authorized
+  const token = "xyz"
+  const isAdminAuthorized = token === "xyz"
+  if(isAdminAuthorized){
+    res.send("All Data sent")
+  }else{
+    res.status(401).send("Unauthorized request");
   }
-);
+})
 
-
-
+app.get("/admin/deleteUser",(req, res)=>{
+  //Logic of Check if the request is authorized
+  res.send("Deleted a user")
+})
 
 app.listen(3000, () => {
   console.log("Server is succesfully listening on port 3000....");
